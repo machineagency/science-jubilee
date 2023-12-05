ff4dd08f4be2286021df5e672e59441b27b7cbcb
Brenden Pelkie <bgpelkie@gmail.com>
2023-12-05 13:33:27 -0800

    finishing up peristaltic pump tool definitions

--- science_jubilee/tools/PeristalticPumps.py
+++ science_jubilee/tools/PeristalticPumps.py
@@ -5,35 +5,26 @@ import os
 from science_jubilee.tools.Tool import Tool, ToolStateError, ToolConfigurationError, requires_active_tool
 from typing import Tuple, Union
 
+class PeristalticPumpIndividual(Tool):
 
+    """Class representation for an individual peristaltic pump. Individual pumps should be combined into a pumps group to work correctly 
+    
+    
+    Really the only reason to have this as its own class is to let you use pumps with different steps per mm"""
 
-class PeristalticPump(Tool):
-    """
-    Class to manage peristaltic pump operation
-    """
+    def __init__(self, steps_per_ml: float, name: str, user_calibrated: bool = False):
 
-    def __init__(self, index, steps_per_ml, ):
+        super().__init__(steps_per_ml = steps_per_ml, name = name, user_calibrated = user_calibrated)
 
-        # load config file
-
-        # check if mm to volume conversion has been calibrated by user
-        # overwrite machine steps per mm with config supplied one
-        # send M92E{value}
-
-       
+        if not user_calibrated:
+            raise Warning('Peristaltic pump not calibrated, accuracy may be inadequate')       
 
     @classmethod
-    def from_config(cls, machine, index, name, config_file: str,
+    def from_config(cls, config_file: str,
                     path :str = os.path.join(os.path.dirname(__file__), 'configs')):
         
         """Initialize the pipette object from a config file
 
-        :param machine: The :class:`Machine` object that the pipette is loaded on
-        :type machine: :class:`Machine`
-        :param index: The tool index of the pipette on the machine
-        :type index: int
-        :param name: The tool name
-        :type name: str
         :param config_file: The name of the config file containign the pipette parameters
         :type config_file: str
         :returns: A :class:`Pipette` object
@@ -43,17 +34,58 @@ class PeristalticPump(Tool):
         with open(config) as f:
             kwargs = json.load(f)
 
-        return cls(machine, index, name, **kwargs)
+        return cls(**kwargs)
 
-    def pump(volume:float, speed: float):
+class PeristalticPumps(Tool):
+    """
+    A class representation of a group of PeristalticPumpIndividual objects. Can instantiate with list of PeristalticPumpIndividual objects or a number of pumps to generate and a config. Generic to any stepper-driven peristaltic pump. Combine with PumpDispenser tool for bulk low-precision liquid handling.
+    """
+
+    def __init__(self, index, pumps = None, n_pumps = None, steps_per_ml = None, config_file = None, tool_axis = 'E'):
+
+        self.index = index
+        
+        if pumps is not None:
+            assert isinstance(pumps, list), '"pumps" must be a list of PeristalticPumpIndividual objects'
+            self.pumps = pumps
+
+        elif n_pumps is not None:
+            assert isinstance(n_pumps, int), '"n_pumps" must be an int'
+            if config_file is not None:
+                pump = PeristalticPumpIndividual.from_config(config_file)
+            elif steps_per_ml is not None:
+                pump = PeristalticPumpIndividual(steps_per_ml, 'generic_pump', user_calibrated=True)
+            self.pumps = [pump]*n_pumps
+
+        self.n_pumps = len(self.pumps)
+
+    def post_load(self):
+        """
+        After tool load, set steps per mm on machine
+        """
+        #set steps per mL on machine
+        steps_per_ml = [str(pump.steps_per_mm) for pump in self.pumps]
+
+        gcode = f"M92 {self.tool_axis}"+':'.join(str(steps_per_ml))
+        self._machine.gcode(gcode)
+
+        return
+
+    @requires_active_tool
+    def pump(self, volume: [float, list], speed: float):
         """turn on pump to dispense volume at speed"""
 
         # calculate 'mm' to dispense given volume
         # actually this is handled in the steps to mm conversion programmed in axis setup on Jubilee 
         
         # dispense given volume
-        volumes = [0,0,0]
-        volumes[pippette_index] = volume
-        stringvol = ':'.join([str(v) for v in volumes]) # negative b/c gcode pump reverse is suspect for now
+        if isinstance(volume, list):
+            pass
 
-        self.gcode(f'G1 E{stringvol}')
+        elif isinstance(volume, float):
+            volume = [volume]*self.n_pumps
+
+        stringvol = ':'.join([str(v) for v in volume]) # negative b/c gcode pump reverse is suspect for now
+
+        # sticking with a direct gcode to send here makes sense: no 3-motor on axis support in movement code, and no risk of crashing anything here
+        self.gcode(f'G1 {self.tool_axis}{stringvol}')
--- science_jubilee/tools/PumpDispenser.py
+++ science_jubilee/tools/PumpDispenser.py
@@ -5,38 +5,117 @@ import os
 from science_jubilee.labware.Labware import Labware, Well, Location
 from science_jubilee.tools.Tool import Tool, ToolStateError, ToolConfigurationError, requires_active_tool
 from typing import Tuple, Union
+from science_jubilee import utils
 
 class PumpDispenser(Tool):
     """
     Class to manage dispenser tool, for example for a color mixing demo
     """
 
-    def init()
+
+    def __init__(self, index, pump_group, dispense_tip_offsets):
+        """
+        parameters for user to give:
+        - number of dispenser heads (only 3 supported for now)
+        - pump:dispenser head mapping
+        - garbage/waste well
+        - line prime/purge volume
+
+
+        This needs to have all the well management tooling of the pipette
+        """
+        super().__init__(index, pump_group = pump_group, dispense_tip_offsets = dispense_tip_offsets)
+
+        self.n_dispense_heads = len(self.dispense_tip_offsets)
+        self.waste = None
+        assert self.pump_group.n_pumps == self.n_dispense_heads, "Number of pumps must match number of dispenser tips"
+
+    def add_waste(self, location: Union[Well, Tuple, Location]):
+        """
+        Specify a waste collection container
+        """
+        assert isinstance(location, Union[Well, Tuple, Location]), 'location must be a well, tuple, or location'
+        self.waste = location
+
+
+    def dispense(self, vol: Union[float, int, list], location:Union[Well, Tuple, Location], dispense_head_index = None):
+        """
+        Dispense volume vol from dispense head dispense_head_index into location 
+        """
+        # get dispense volumes for each dispense head 
+        if isinstance(vol, list):
+            assert len(vol) == self.n_dispense_heads
+            dispense_volumes = vol
+        elif isinstance(vol, float) or isinstance(vol, int):
+            if dispense_head_index is None:
+                # dispense same volume from every head
+                dispense_volumes = [vol]*self.n_dispense_heads
+            elif isinstance(dispense_head_index, int):
+                volumes = [0]*self.n_dispense_heads
+                volumes[dispense_head_index] = vol
+            else:
+                raise AssertionError('dispense_head_index must be an integer') 
+        else:
+            raise AssertionError('vol must be a float, int, or list')
+
+
+        # calculate XY location for each dispense head
+        x, y, z = utils.getxyz(location)
+
+        if type(location) == Well:
+            if z == location.z:
+                z = z + 10
+            else:
+                pass
+        else:
+            pass
+
+        # iterate over each dispense head, skip if 0. Move to location and dispense 
+        for i, vol in enumerate(volumes):
+            if vol == 0:
+                continue
+            else:
+                tip_offsets = self.dispense_tip_offsets[i]
+                x_tip = x - tip_offsets[0]
+                y_tip = y - tip_offsets[1]
+
+                self._machine.safe_z_movement()
+                self._machine.move_to(x = x_tip, y = y_tip)
+                self._machine.move_to(z = z)
+
+                self.pump_group.pump(dispense_volumes)
+
         
-        # offsets live in config file, pipette tip '0' is centered in Jubilee duet-level setup
-        
-
-    """
-    parameters for user to give:
-    - number of dispenser heads (only 3 supported for now)
-    - pump:dispenser head mapping
-    - garbage/waste well
-    - line prime/purge volume
 
 
-    This needs to have all the well management tooling of the pipette
-    """
-
-    def dispense(well, volume, speed, dispense head):
-
-
-    def prime_lines(volume):
+    def prime_lines(self, volume: Union[int, float] = None, location:Union[Well, Tuple, Location] = None):
         """
         fill lines with liquid from all pumps at same time, dispense `volume` from each pump
         """
 
-    def empty_lines(volume):
+        if volume is None:
+            try:
+                volume = self.prime_line_volume
+            except AttributeError:
+                raise AssertionError('Line prime volume not specified in configuration. Please specify a volume')
+        
+        if location is None and self.waste is None:
+            raise AssertionError('Please specify a waste location')
+        
+        self.dispense(volume, location)
+
+        
+    def empty_lines(self, volume: Union[int, float] = None):
         """
         return line contents to stock wells
         """
+        if volume is None:
+            try:
+                volume = self.prime_line_volume
+            except AttributeError:
+                raise AssertionError('Line prime volume not specified in configuration. Please specify a volume')
+
+        self.pump_group.pump(-volume)
+
+
 
